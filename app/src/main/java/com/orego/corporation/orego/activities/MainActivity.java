0b39dcfc2aac856001e24e1bf2a76d822edec5a9
darksithnanana <lada.dolgusheva@yandex.ru>
2018-08-22 14:00:20 +0300

    gradle sync

--- app/src/main/java/com/orego/corporation/orego/activities/MainActivity.java
+++ app/src/main/java/com/orego/corporation/orego/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.orego.corporation.orego;
+package com.orego.corporation.orego.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.orego.corporation.orego.R;
 
 public class MainActivity extends AppCompatActivity {
 
